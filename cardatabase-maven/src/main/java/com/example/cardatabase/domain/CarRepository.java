2dcf55b3e4a9d7a2f12f5a3a07aecebacacfc93d
Ismael Ait <IsmaelDavid.Ait@endava.com>
2021-12-14 16:09:51 -0300

    Add Spring Boot Data Rest

--- cardatabase-maven/src/main/java/com/example/cardatabase/domain/CarRepository.java
+++ cardatabase-maven/src/main/java/com/example/cardatabase/domain/CarRepository.java
@@ -4,21 +4,24 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CarRepository extends PagingAndSortingRepository<Car, Long> {
 
 	@Query("select c from Car c where c.brand = ?1")
-	List<Car> findByBrand(String brand);
+	List<Car> findByBrand(@Param("brand") String brand);
 
 	@Query("select c from Car c where c.brand like %?1")
-	List<Car> findByBrandEndsWith(String brand);
+	List<Car> findByBrandEndsWith(@Param("brand") String brand);
 
-	List<Car> findByBrandOrderByYearAsc(String brand);
+	List<Car> findByBrandOrderByYearAsc(@Param("brand") String brand);
 
-	List<Car> findByBrandAndModel(String brand, String model);
+	List<Car> findByBrandAndModel(@Param("brand") String brand, @Param("model") String model);
 
-	List<Car> findByColor(String color);
+	List<Car> findByColor(@Param("color") String color);
 
-	List<Car> findByYear(Integer year);
+	List<Car> findByYear(@Param("year") Integer year);
 
 }
