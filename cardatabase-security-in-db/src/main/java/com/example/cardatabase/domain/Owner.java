22fc6503eaa2ab854930aeb18c19664153cec988
Ismael Ait <IsmaelDavid.Ait@endava.com>
2021-12-16 10:01:42 -0300

    Create project with security storing the users in the database

---
+++ cardatabase-security-in-db/src/main/java/com/example/cardatabase/domain/Owner.java
@@ -0,0 +1,39 @@
+package com.example.cardatabase.domain;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Owner {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String firstName;
+
+	private String lastName;
+
+	// cascade ALL means that if the owner is deleted, the cars linked to that owner are deleted as well
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+	@JsonIgnore
+	private List<Car> cars;
+
+}
